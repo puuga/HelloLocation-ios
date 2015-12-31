db5daea197766a2dd54c33c37766872a4e59896e
Siwawes Wongcharoen <siwaoh@gmail.com>
2015-12-31 09:46:42 +0700

    Initial Commit

---
+++ HelloLocation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloLocation
+//
+//  Created by Siwawes Wongcharoen on 12/31/2558 BE.
+//  Copyright © 2558 puuga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
